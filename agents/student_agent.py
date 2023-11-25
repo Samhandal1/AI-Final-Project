d40ea7dc8812b6c88a79fb553852f5eee820e7d3
ameliebarsoum <amelie.barsoum@mail.mcgill.ca>
2023-11-25 16:42:05 -0500

    started BFS and initial check_endgame method

--- agents/student_agent.py
+++ agents/student_agent.py
@@ -5,6 +5,7 @@ import sys
 import numpy as np
 from copy import deepcopy
 import time
+import random
 
 
 @register_agent("student_agent")
@@ -39,14 +40,146 @@ class StudentAgent(Agent):
 
         Please check the sample implementation in agents/random_agent.py or agents/human_agent.py for more details.
         """
+        # Taken for random_agent.py
+        def __init__(self):
+        super(HumanAgent, self).__init__()
+        self.name = "HumanAgent"
+        self.dir_map = {
+            "u": 0,
+            "r": 1,
+            "d": 2,
+            "l": 3,
+        }
+
 
         # Some simple code to help you with timing. Consider checking 
         # time_taken during your search and breaking with the best answer
         # so far when it nears 2 seconds.
         start_time = time.time()
+
+        # logic goes here
+
+
         time_taken = time.time() - start_time
         
         print("My AI's turn took ", time_taken, "seconds.")
 
         # dummy return
         return my_pos, self.dir_map["u"]
+
+
+    def BFS(self, chess_board, my_pos, adv_pos, max_step) :
+
+        # Moves (Up, Right, Down, Left) 
+        # (0, 1, 2, 3)
+        moves = ((-1, 0), (0, 1), (1, 0), (0, -1))
+
+        # Keep track of players locations and barrier locations
+
+        # Check that players are not randomly positioned in same cell
+        if (my_pos[0] == adv_pos[1] and my_pos[1] == adv_pos[1]) :
+            return False
+
+        # BFS from world.py
+        state_queue = [(start_pos, 0)]
+        visited = {tuple(start_pos)}
+        is_reached = False
+        while state_queue and not is_reached:
+            cur_pos, cur_step = state_queue.pop(0)
+            r, c = cur_pos
+            if cur_step == self.max_step:
+                break
+            for dir, move in enumerate(self.moves):
+                if self.chess_board[r, c, dir]:
+                    continue
+
+                next_pos = cur_pos + move
+                if np.array_equal(next_pos, adv_pos) or tuple(next_pos) in visited:
+                    continue
+                if np.array_equal(next_pos, end_pos):
+                    is_reached = True
+                    break
+
+                visited.add(tuple(next_pos))
+                state_queue.append((next_pos, cur_step + 1))
+
+        return is_reached
+
+
+        
+    def is_barrier(self, chess_board, position):
+        x, y = position
+        return np.any(chess_board[x, y])
+
+    def check_endgame(self, chess_board, p0_pos, p1_pos):
+        """
+        Check if the game ends and compute the current score of the agents.
+
+        Returns
+        -------
+        is_endgame : bool
+            Whether the game ends.
+        player_1_score : int
+            The score of player 1.
+        player_2_score : int
+            The score of player 2.
+        """
+
+        moves = ((-1, 0), (0, 1), (1, 0), (0, -1))
+        board_size = len(chess_board[0])
+
+        # Union-Find
+        father = dict()
+        for r in range(board_size):
+            for c in range(board_size):
+                father[(r, c)] = (r, c)
+
+        def find(pos):
+            if father[pos] != pos:
+                father[pos] = find(father[pos])
+            return father[pos]
+
+        def union(pos1, pos2):
+            father[pos1] = pos2
+
+        for r in range(board_size):
+            for c in range(board_size):
+                for dir, move in enumerate(moves[1:3]):  # Only check down and right
+                    if chess_board[r, c, dir + 1]:
+                        continue
+                    #########Add into else block with more logic
+                    pos_a = find((r, c))
+                    pos_b = find((r + move[0], c + move[1]))
+                    if pos_a != pos_b:
+                        union(pos_a, pos_b)
+
+                    ##########
+
+        for r in range(board_size):
+            for c in range(board_size):
+                find((r, c))
+        p0_r = find(tuple(p0_pos))
+        p1_r = find(tuple(p1_pos))
+        p0_score = list(father.values()).count(p0_r)
+        p1_score = list(father.values()).count(p1_r)
+        if p0_r == p1_r:
+            return False, p0_score, p1_score
+        player_win = None
+        win_blocks = -1
+        if p0_score > p1_score:
+            player_win = 0
+            win_blocks = p0_score
+        elif p0_score < p1_score:
+            player_win = 1
+            win_blocks = p1_score
+        else:
+            player_win = -1  # Tie
+        if player_win >= 0:
+            logging.info(
+                f"Game ends! Player {self.player_names[player_win]} wins having control over {win_blocks} blocks!"
+            )
+        else:
+            logging.info("Game ends! It is a Tie!")
+        return True, p0_score, p1_score
+
+
