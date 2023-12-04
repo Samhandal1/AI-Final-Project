9748edbb853ff0bd7b34f7cc81c5885f10c0256c
Samhandal1 <samantha.handal@mail.mcgill.ca>
2023-12-03 21:56:01 -0500

    FINALLY GOOOOOD

--- agents/student_agent.py
+++ agents/student_agent.py
@@ -1,10 +1,8 @@
 # Student agent: Add your own agent here
 from agents.agent import Agent
 from store import register_agent
-import sys
 import numpy as np
 from copy import deepcopy
-import time
 
 
 @register_agent("student_agent")
@@ -24,43 +22,49 @@ class StudentAgent(Agent):
             "l": 3,
         }
 
-    def evaluate(self, chess_board, my_pos, adv_pos):
+    def evaluate(self, chess_board, my_pos, my_dir, adv_pos):
         score = 0
 
-        # 1. Check for being surrounded by three walls
+        # 1. Immediate threat of being trapped
         if self.is_three_sided(my_pos, chess_board):
-            score -= 100
+            score -= 500  # Heavy penalty for being nearly trapped
 
-        # 2. Count number of move options
+        # 2. Evaluate the possibility of trapping the opponent
+        if self.is_three_sided(adv_pos, chess_board):
+            score += 500  # High reward for nearly trapping the opponent
+
+        # 3. Mobility - Number of move options
         my_move_options = self.count_move_options(my_pos, chess_board)
-        score += my_move_options * 5  # Mobility is important
+        score += my_move_options * 10
 
-        # 3. Calculate distance from the opponent
+        # 4. Opponent's mobility
+        opp_move_options = self.count_move_options(adv_pos, chess_board)
+        score -= opp_move_options * 10
+
+        # 5. Distance from the opponent - less emphasis as depth is 1
         distance_to_opponent = self.calculate_distance(my_pos, adv_pos)
-        # In early and middle game, maintain a moderate distance
-        score += max(5 - distance_to_opponent, 0) * 3
+        score -= distance_to_opponent * 5
 
-        # 4. Assess the game stage by the number of walls
-        total_walls = np.sum(chess_board)
-        board_size = chess_board.shape[0]
-        early_game_threshold = board_size * 2  # Early game if fewer than 2 walls per row
-        middle_game_threshold = board_size * 4  # Middle game up to 4 walls per row
+        # 6. Check if the wall placement extends a wall
+        if self.is_extending_wall(my_pos, my_dir, chess_board):
+            score += 100  # Reward for extending a wall
 
-        if total_walls < early_game_threshold:
-            score += my_move_options * 3  # Focus on mobility in the early game
-        elif total_walls < middle_game_threshold:
-            score += (10 - distance_to_opponent) * 2  # Begin aggressive play
-        else:
-            score += (board_size**2 - total_walls) * 2  # Focus on area control in the end game
+        # 7. Check if the wall placement helps in trapping the opponent
+        if self.is_trapping_opponent(my_pos, my_dir, adv_pos, chess_board):
+            score += 200  # Additional reward for moves that help in trapping the opponent
 
-        # 5. Check for continuing a wall pattern
-        if self.is_continuing_wall(my_pos, chess_board):
-            score += 10  # Reward for strategic wall placement
+        # 8. Evaluate wall placement in terms of cutting off areas
+        accessible_area_proportion = self.does_wall_cut_off_area(my_pos, my_dir, chess_board, adv_pos)
+        # Calculate the score based on the area cut off
+        # For example, if 70% of the area is cut off, subtract 70% of a maximum cutoff score
+        max_cutoff_score = 300  # Maximum score for completely cutting off the opponent
+        score += (1 - accessible_area_proportion) * max_cutoff_score
 
         return score
 
+    ### Helper functions for evaluate (implement these based on game logic) ###
 
-    # Helper functions for evaluate (implement these based on game logic)
+    # 1. Check for being surrounded by 3 walls
     def is_three_sided(self, pos, chess_board):
         r, c = pos
         sides_with_walls = 0
@@ -76,7 +80,7 @@ class StudentAgent(Agent):
 
         return sides_with_walls == 3
 
-
+    # 2. Check for how many move options you would have after ending
     def count_move_options(self, pos, chess_board):
         r, c = pos
         move_options = 0
@@ -92,159 +96,206 @@ class StudentAgent(Agent):
 
         return move_options
 
+    # 3. Check your distance from opponent
     def calculate_distance(self, pos1, pos2):
+
         # Manhattan distance
         return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
 
-    def is_continuing_wall(self, pos, chess_board):
+    # 4. “continuing” a wall or pattern is more effective than moving to purely free space
+    def is_extending_wall(self, pos, dir, chess_board):
+        # Determine if placing a wall in the given direction extends an existing wall
         r, c = pos
 
         # Moves (Up, Right, Down, Left)
         moves = ((-1, 0), (0, 1), (1, 0), (0, -1))
 
-        for i, move in enumerate(moves):
-            new_r, new_c = r + move[0], c + move[1]
-            if 0 <= new_r < chess_board.shape[0] and 0 <= new_c < chess_board.shape[1]:
-                if chess_board[new_r, new_c, (i + 2) % 4]:  # Check opposite direction for a continuation
-                    return True
-
+        # Check the adjacent cell in the direction of the wall placement
+        new_r, new_c = r + moves[dir][0], c + moves[dir][1]
+        if 0 <= new_r < chess_board.shape[0] and 0 <= new_c < chess_board.shape[1]:
+            # Check if there's already a wall in the opposite direction
+            return chess_board[new_r, new_c, (dir + 2) % 4]
         return False
+    
+    # 5. predicting whether placing a wall in a given direction will significantly limit the opponent's mobility or potentially lead to their entrapment
+    def is_trapping_opponent(self, my_pos, my_dir, adv_pos, chess_board):
+        # Simulate placing the wall
+        new_chess_board = deepcopy(chess_board)
+        r, c = my_pos
+        new_chess_board[r, c, my_dir] = True
+
+        # Count the opponent's move options before and after placing the wall
+        original_move_options = self.count_move_options(adv_pos, chess_board)
+        new_move_options = self.count_move_options(adv_pos, new_chess_board)
+
+        # If the number of move options is significantly reduced, return True
+        return new_move_options < original_move_options
+    
+    # 6. Score depend on the portion of the area cut off by the wall placement
+    def does_wall_cut_off_area(self, pos, dir, chess_board, adv_pos):
+        # Place the wall
+        new_chess_board = deepcopy(chess_board)
+        r, c = pos
+        new_chess_board[r, c, dir] = True
+
+        # Perform a flood fill from the opponent's position
+        accessible_area = self.flood_fill(adv_pos, new_chess_board)
+        total_area = chess_board.shape[0] * chess_board.shape[1]
+
+        # Return the proportion of the accessible area
+        return accessible_area / total_area
+
+    def flood_fill(self, start_pos, chess_board):
+        queue = [start_pos]
+        visited = set()
+        directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]  # Up, Right, Down, Left
+
+        while queue:
+            r, c = queue.pop(0)
+            if (r, c) in visited:
+                continue
+            visited.add((r, c))
+
+            for i, (dr, dc) in enumerate(directions):
+                new_r, new_c = r + dr, c + dc
+                if 0 <= new_r < chess_board.shape[0] and 0 <= new_c < chess_board.shape[1]:
+                    if not chess_board[r, c, i]:  # Check if no wall in the direction
+                        queue.append((new_r, new_c))
+
+        return len(visited)
+    
+    # 7. Counts barrier, good indicator for game development
+    def count_barriers(self, chess_board):
+        barrier_count = 0
+        for row in chess_board:
+            for cell in row:
+                # Summing the boolean values for barriers in each direction
+                barrier_count += sum(cell)  
+                
+        return barrier_count
+
+    # 8. Check for end game
+    def is_end_game_start(self, chess_board):
+
+        board_size = chess_board.shape[0] * chess_board.shape[1] * chess_board.shape[2]
+        barrier_count = self.count_barriers(chess_board)
+
+        # Calculate the percentage of the board occupied by barriers
+        percentage_occupied = (barrier_count / board_size) * 100
+
+        # Define a threshold percentage to mark the start of the end game
+        end_game_threshold = 28  # This is an example value, adjust based on your game dynamics
+
+        return percentage_occupied >= end_game_threshold
+    
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# # # # #                                                           # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# # # # #   ab - pruning logic, implemented in minimax() function   # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# # # # #                                                           # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
     def minimax(self, chess_board, depth, is_maximizing_player, my_pos, adv_pos, max_step, alpha, beta):
+    
+        isEndGame, my_score, adv_score = self.check_endgame(chess_board, my_pos, adv_pos)
 
-        if depth == 0:
-            return self.evaluate(chess_board, my_pos, adv_pos)
+        # Terminate if it's endgame or depth is 0
+        if isEndGame or depth == 0: 
+            if my_score > adv_score:
+                return my_score, None
+            elif my_score < adv_score:
+                return -adv_score, None
+            else:
+                return 0, None
 
         if is_maximizing_player:
             max_eval = float('-inf')
-            for move in self.generate_moves(chess_board, my_pos, adv_pos, max_step):
-
+            best_move = None
+            moves = self.generate_moves(chess_board, my_pos, adv_pos, max_step)
+            for move in self.best_guess_moves(moves, adv_pos, chess_board):
                 new_pos, barrier_dir = move
-                r, c = new_pos
-
                 new_chess_board = deepcopy(chess_board)
-                new_chess_board[r, c, barrier_dir] = True
+                new_chess_board[new_pos[0], new_pos[1], barrier_dir] = True
 
-                eval = self.minimax(new_chess_board, depth - 1, False, new_pos, adv_pos, max_step, alpha, beta)
-                max_eval = max(max_eval, eval)
-                alpha = max(alpha, max_eval)
+                eval, _ = self.minimax(new_chess_board, depth - 1, False, new_pos, adv_pos, max_step, alpha, beta)
+                if eval > max_eval:
+                    max_eval = eval
+                    best_move = move
+                alpha = max(alpha, eval)
                 if beta <= alpha:
                     break
-            return max_eval
+            return max_eval, best_move
         else:
             min_eval = float('inf')
-            for move in self.generate_moves(chess_board, adv_pos, my_pos, max_step):
-                
+            best_move = None
+            moves = self.generate_moves(chess_board, adv_pos, my_pos, max_step)
+            for move in self.best_guess_moves(moves, my_pos, chess_board):
                 new_pos, barrier_dir = move
-                r, c = new_pos
-                
                 new_chess_board = deepcopy(chess_board)
-                new_chess_board[r, c, barrier_dir] = True
-                
-                eval = self.minimax(new_chess_board, depth - 1, True, my_pos, new_pos, max_step, alpha, beta)
-                min_eval = min(min_eval, eval)
-                beta = min(beta, min_eval)
+                new_chess_board[new_pos[0], new_pos[1], barrier_dir] = True
+
+                eval, _ = self.minimax(new_chess_board, depth - 1, True, my_pos, new_pos, max_step, alpha, beta)
+                if eval < min_eval:
+                    min_eval = eval
+                    best_move = move
+                beta = min(beta, eval)
                 if beta <= alpha:
                     break
-            return min_eval
+            return min_eval, best_move
 
+        
     def step(self, chess_board, my_pos, adv_pos, max_step):
-        best_move = None
-        best_score = float('-inf')
-        depth = 0
 
-        alpha = float('-inf')
-        beta = float('inf')
+        if self.is_end_game_start(chess_board):
+            best_move = None
+            depth = 0
 
-        if chess_board.shape[0] < 8:
-            depth = 1
-        elif chess_board.shape[0] >= 8 and chess_board.shape[0] <= 10:
-            depth = 1
+            alpha = float('-inf')
+            beta = float('inf')
+
+            if chess_board.shape[0] < 8:
+                depth = 7
+            elif chess_board.shape[0] >= 8 and chess_board.shape[0] <= 10:
+                depth = 5
+            else:
+                depth = 3
+
+            _, best_move = self.minimax(chess_board, depth, True, my_pos, adv_pos, max_step, alpha, beta)
+            if best_move is None:
+                moves = self.generate_moves(chess_board, my_pos, adv_pos, max_step)
+                best_moves = self.best_guess_moves(moves, adv_pos, chess_board)
+                best_move = best_moves[0]
+
+            return best_move
         else:
-            depth = 1
+            moves = self.generate_moves(chess_board, my_pos, adv_pos, max_step)
+            best_move = self.best_guess_moves(moves, adv_pos, chess_board)
 
-        # Generate all possible moves and apply minimax
-        for move in self.generate_moves(chess_board, my_pos, adv_pos, max_step):
-            score = self.minimax(chess_board, depth, True, my_pos, adv_pos, max_step, alpha, beta)
-            if score > best_score:
-                best_score = score
-                best_move = move
+            return best_move[0]
+        
 
-        return best_move
-    
-    def generate_moves_2(self, chess_board, my_pos, adv_pos, max_step):
+    def best_guess_moves(self, moves, adv_pos, chess_board):
+        best_guess_moves_score = []
+        for index, move in enumerate(moves):
+            score = self.evaluate(chess_board, move[0], move[1], adv_pos)
+            best_guess_moves_score.append((score, index))
 
-        # Moves (Up, Right, Down, Left)
-        moves = ((-1, 0), (0, 1), (1, 0), (0, -1))
+        # Sort the moves by their scores in descending order
+        best_guess_moves_score.sort(reverse=True, key=lambda x: x[0])
 
-        possible_moves = []
+        # Get the indices of the top 10 moves
+        top_indices = [move_score[1] for move_score in best_guess_moves_score[:3]]
 
-        # Generate all possible positions after moving up to max_step steps
-        for step in range(0, max_step + 1):
-            for move in moves:
-                new_r = my_pos[0] + move[0] * step
-                new_c = my_pos[1] + move[1] * step
-                if self.check_valid_step(my_pos, (new_r, new_c), adv_pos, moves.index(move), max_step, chess_board):
-                    possible_moves.append(((new_r, new_c), moves.index(move)))
+        # Retrieve the top 3 moves using the indices
+        top_moves = [moves[index] for index in top_indices]
 
-        return possible_moves
-    
-
-    def check_valid_step(self, start_pos, end_pos, adv_pos, barrier_dir, max_step, chess_board):
-        """
-        Check if the step the agent takes is valid (reachable and within max steps).
-
-        Parameters
-        ----------
-        start_pos : tuple
-            The start position of the agent.
-        end_pos : np.ndarray
-            The end position of the agent.
-        barrier_dir : int
-            The direction of the barrier.
-        """
-
-        # Moves (Up, Right, Down, Left)
-        moves = ((-1, 0), (0, 1), (1, 0), (0, -1))
-
-        # Endpoint already has barrier or is border
-        r, c = end_pos
-        if chess_board[r, c, barrier_dir]:
-            return False
-        if np.array_equal(start_pos, end_pos):
-            return True
-
-        # BFS
-        state_queue = [(start_pos, 0)]
-        visited = {tuple(start_pos)}
-        is_reached = False
-        while state_queue and not is_reached:
-            cur_pos, cur_step = state_queue.pop(0)
-            r, c = cur_pos
-            if cur_step == max_step:
-                break
-            for dir, move in enumerate(moves):
-                if chess_board[r, c, dir]:
-                    continue
-
-                next_pos = cur_pos + move
-                if np.array_equal(next_pos, adv_pos) or tuple(next_pos) in visited:
-                    continue
-                if np.array_equal(next_pos, end_pos):
-                    is_reached = True
-                    break
-
-                visited.add(tuple(next_pos))
-                state_queue.append((next_pos, cur_step + 1))
-
-        return is_reached
+        return top_moves
+        
     
     def generate_moves(self, chess_board, my_pos, adv_pos, max_step):
-
         # Moves (Up, Right, Down, Left)
         moves = ((-1, 0), (0, 1), (1, 0), (0, -1))
 
-        possible_moves = []
+        possible_moves = set()
 
         # Generate all possible positions after moving up to max_step steps
         for step in range(0, max_step + 1):
@@ -257,15 +308,15 @@ class StudentAgent(Agent):
                     0 <= new_c < chess_board.shape[1] and
                     not (new_r, new_c) == adv_pos and
                     self.is_path_clear(chess_board, my_pos, (new_r, new_c), adv_pos, move, moves.index(move))):
-                    
+
                     # For each valid position, generate possible barrier placements
                     for dir in range(4):
-
                         # Check if it's possible to place a barrier in this direction
                         if not chess_board[new_r, new_c, dir]:
-                            possible_moves.append(((new_r, new_c), dir))
+                            possible_moves.add(((new_r, new_c), dir))
 
-        return possible_moves
+        return list(possible_moves)
+
 
     def is_path_clear(self, chess_board, start_pos, end_pos, adv_pos, move, dir):
 
@@ -282,3 +333,54 @@ class StudentAgent(Agent):
             c += move[1]
 
         return True
+    
+
+    def check_endgame(self, chess_board, p0_pos, p1_pos):
+        """
+        ** edit end game function from world.py **
+        Check if the game ends.
+
+        Returns
+        -------
+        is_endgame : bool
+            Whether the game ends.
+        """
+
+        # Moves (Up, Right, Down, Left)
+        moves = ((-1, 0), (0, 1), (1, 0), (0, -1))
+        board_size = chess_board.shape[0]
+
+        # Union-Find
+        father = dict()
+        for r in range(board_size):
+            for c in range(board_size):
+                father[(r, c)] = (r, c)
+
+        def find(pos):
+            if father[pos] != pos:
+                father[pos] = find(father[pos])
+            return father[pos]
+
+        def union(pos1, pos2):
+            father[pos1] = pos2
+
+        for r in range(board_size):
+            for c in range(board_size):
+                for dir, move in enumerate(moves[1:3]):  # Only check down and right
+                    if chess_board[r, c, dir + 1]:
+                        continue
+                    pos_a = find((r, c))
+                    pos_b = find((r + move[0], c + move[1]))
+                    if pos_a != pos_b:
+                        union(pos_a, pos_b)
+
+        for r in range(board_size):
+            for c in range(board_size):
+                find((r, c))
+        p0_r = find(tuple(p0_pos))
+        p1_r = find(tuple(p1_pos))
+        p0_score = list(father.values()).count(p0_r)
+        p1_score = list(father.values()).count(p1_r)
+
+        # Game ends if players are in different regions
+        return (p0_r != p1_r), p0_score, p1_score
